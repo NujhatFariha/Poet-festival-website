fe87d6c3f391f648c94b81ee0b1473fbd1a8e94b
nujhatfariha049 <nujhatfarihaa@gmail.com>
2021-09-28 11:36:20 +0600

    final commit

--- src/components/Poet/Poet.js
+++ src/components/Poet/Poet.js
@@ -6,17 +6,17 @@ const Poet = (props) => {
     const {name, img, dateOfBirth, totalBook, bookPrice, dateOfDeath} = props.poet || {}
 
     return (
-        <div className="information-container">
+        <div className="information-container p-3">
             <div>
             <div className='images'>
-               <img style={{width:'150px',margin:'10px 65px'}} src={img} alt="" />
+               <img style={{width:'150px',margin:'10px 65px', height: '150px'}} src={img} alt="" />
             </div>
             <div>
                <h4>{name}</h4>
-               <p>Birth:{dateOfBirth}</p>
-               <p>Death:{dateOfDeath}</p>
-               <p>Total Book:{totalBook}</p>
-               <p>Average Book Price:{bookPrice}</p>
+               <p>Birth: {dateOfBirth}</p>
+               <p>Death: {dateOfDeath}</p>
+               <p>Total Book: {totalBook}</p>
+               <p>Average Book Price: {bookPrice}</p>
                <button 
                onClick = { () => props.handleAddToCart(props.poet)}
                className = 'regular-btn'><i class="fas fa-shopping-cart"></i> add to cart</button>
